4f0b0abd12821049ad681baa7df4a7c0674d355e
Mello Birkan <mgbirkan@icloud.com>
2024-02-25 15:58:05 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Marcello Gonzatto Birkan on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
