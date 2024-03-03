39699abcbaee017b9d34e4dbe03a99dc0981c4ef
Mello Birkan <mgbirkan@icloud.com>
2024-03-03 02:50:13 -0300

    ScrumView melhorada adicionada na NavigationStack permitindo uma navegação entre as views.

--- Scrumdinger/Views/ScrumView.swift
+++ Scrumdinger/Views/ScrumView.swift
@@ -10,13 +10,23 @@ import SwiftUI
 struct ScrumView: View {
   let scrums: [DailyScrum]
   
-    var body: some View {
-      List(scrums, id: \.title) {scrum in
-        CardView(scrum: scrum)
-          .listRowBackground(scrum.theme.mainColor)
-        
+  var body: some View {
+    NavigationStack {
+      List(scrums) {scrum in
+        NavigationLink(destination: Text(scrum.title)) {
+          CardView(scrum: scrum)
+        }
+        .listRowBackground(scrum.theme.mainColor)
+      }
+      .navigationTitle("Daily Scrums")
+      .toolbar {
+        Button(action: {}, label: {
+          Image(systemName: "plus")
+        })
+        .accessibilityLabel("New Scrum")
       }
     }
+  }
 }
 
 #Preview {
